2c1d9b4cf1d0b07fa465014c1e11516d1807f18d
h1t3sh <hitesh.l33t@gmail.com>
2020-04-27 17:30:10 +0700

    Added campgrounds Page

--- app.js
+++ app.js
@@ -11,10 +11,16 @@ app.get("/", function (req, res){
 	res.render("home");
 });
 
-
-
-
-
+// Camprounds
+app.get("/campgrounds", function(req, res){
+	// We start with Array for now, we'll connect a DB later
+	var campgrounds = [
+		{name: "Jane's Mountain", image="https://pixabay.com/get/57e1d14a4e52ae14f1dc84609620367d1c3ed9e04e507441722673d79248c3_340.jpg"},
+		{name: " The Solomen's den", image="https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf85254794e7c2778d1924b_340.jpg"},
+		{name: "Canyon Bay", image="https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e507441722673d79248c3_340.jpg"}
+	]
+	res.render("campgrounds",{campgrounds: campgrounds});
+});
 
 
 
