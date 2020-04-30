4e6aecb8bbf75483b3545ec198956ce6396becfe
John <orange.c13@yahoo.com>
2020-04-30 20:22:07 +0530

    Some more styling/formatting

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var campgrounds = [
 	{name: "Canyon Bay", image:"https://images.unsplash.com/photo-1494112142672-801c71472ba5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
 	{name: " The Solomen's den", image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547941772b72d5954f_340.jpg"},
 	{name: "Canyon Bay", image:"https://images.unsplash.com/photo-1494112142672-801c71472ba5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+	{name: "Canyon Bay", image:"https://images.unsplash.com/photo-1494112142672-801c71472ba5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
 	{name: "Jane's Mountain", image:"https://pixabay.com/get/57e1d14a4e52ae14f1dc84609620367d1c3ed9e04e5074417d2d7fdd964fc7_340.jpg"},
 	{name: " The Solomen's den", image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547941772b72d5954f_340.jpg"},
 	{name: "Canyon Bay", image:"https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e5074417d2d7fdd914cc2_340.jpg"}
