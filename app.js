0581fc366b4f235633618424a0b2ee039b739ecf
h1t3sh <hitesh.l33t@gmail.com>
2020-05-26 18:43:45 +0530

    Final commit of version2-mongo branch

--- app.js
+++ app.js
@@ -20,34 +20,6 @@ var campgroundSchema = new mongoose.Schema({
 // Make a model from Schema, so that we can use useful methods
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-// Temporary Code
-//Campground.create(
-//    {
-//        name: "Canyon Bay", 
-//        image:"https://images.unsplash.com/photo-1494112142672-801c71472ba5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-//        description: "A Beautiful Bay with an Awesome sunset View."
-//    }, function(err,campground){
-//        if(err){
-//            console.log(err);
-//        }else{
-//            console.log("Newly Created Campground");
-//            console.log(campground)
-//        }
-//    });
-
-
-// We start with Array for now, we'll connect a DB later
-//var campgrounds = [
-//	{name: "Jane's Mountain", image:"https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"},
-//	{name: "Canyon Bay", image:"https://images.unsplash.com/photo-1494112142672-801c71472ba5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-//	{name: " The Solomen's den", image:"https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-//	{name: "Jane's Mountain", image:"https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"},
-//	{name: "Canyon Bay", image:"https://images.unsplash.com/photo-1494112142672-801c71472ba5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-//	{name: "Canyon Bay", image:"https://images.unsplash.com/photo-1494112142672-801c71472ba5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-//	{name: "Jane's Mountain", image:"https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"},
-//	{name: " The Solomen's den", image:"https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-//	{name: "Canyon Bay", image:"https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
-//];
 
 // ROUTES
 
