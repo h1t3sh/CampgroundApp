79ccc9aca2257b8da65877e81d40b0083c5009d7
h1t3sh <hitesh.l33t@gmail.com>
2020-07-06 19:59:47 +0530

    Added the authorization control on updating/deleting campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -58,19 +58,16 @@ router.get("/:id", function(req, res){
 });
 
 // EDIT CAMPGROUND ROUTE
-router.get("/:id/edit", function(req, res){
+router.get("/:id/edit", checkCamgproundOwnership, function(req, res){
+    // if v reach this point then there should not be error
     Campground.findById(req.params.id, function(err, foundCampground){  // first we check if we have a campground with that id (think req.params.edit) was available
-        if (err){
-            res.redirect("/campgrounds")
-        }else{
-            res.render("campgrounds/edit", {campground: foundCampground});
-        }
-    })
+        res.render("campgrounds/edit", {campground: foundCampground});
+    });
 });
 
 
 // UPDATE CAMPGROUND ROUTE
-router.put("/:id",  function(req, res){
+router.put("/:id", checkCamgproundOwnership, function(req, res){
     // Find and update correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
         if(err){
@@ -84,7 +81,7 @@ router.put("/:id",  function(req, res){
 })
 
 // DESTROY CAMPGROUND ROUTE
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCamgproundOwnership, function(req, res){
     Campground.findByIdAndRemove(req.params.id, function(err){
         if(err){
             res.redirect("/campgrounds");
@@ -103,4 +100,51 @@ function isLoggedIn(req, res, next){
     res.redirect("/login");
 }
 
+function checkCamgproundOwnership(req, res, next){
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+            if(err){
+                res.redirect("back");
+            }else{
+                // does user own campground?
+                if(foundCampground.author.id.equals(req.user._id)){
+                    next();
+                }else{
+                    res.redirect("back");
+                }
+            }
+        })
+    }else{
+        res.redirect("back"); // send user back to that page only
+    }
+}
+
 module.exports = router;
+
+
+//// EDIT CAMPGROUND ROUTE
+//router.get("/:id/edit", function(req, res){
+//    // is user logged in?
+//    if(req.isAuthenticated()){  //req.isAuthenticated() will return true if user is logged in
+//        Campground.findById(req.params.id, function(err, foundCampground){  // first we check if we have a campground with that id (think req.params.edit) was available
+//            if (err){
+//                res.redirect("/campgrounds")
+//            }else{
+//        // if logged in, does user own campground?, if yes then run the code below
+//                    console.log(foundCampground.author.id); //an object
+//                    console.log(req.user._id);  // a string
+////                if(foundCampground.author.id === req.user._id)
+//                if(foundCampground.author.id.equals(req.user._id)){
+//                res.render("campgrounds/edit", {campground: foundCampground});
+//                }else{
+//                    res.send("You donot have permission to do that!")
+//                }
+//            }
+//        });
+//    } else {
+//        console.log("You need to be logged in to do that!!!")
+//        res.send("You need to be logged in to do that!!!")
+//    }
+//        // else redirect  
+//    // if not redirect somewhere
+//});
