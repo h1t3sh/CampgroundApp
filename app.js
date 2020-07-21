a8c1a330db5e9fcb4b263d95e81b8211a93fd3e5
h1t3sh <hitesh.l33t@gmail.com>
2020-07-22 01:04:33 +0530

    add dynamic listening port in addition to static one

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 
 // Start Server port (3000)
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("Campground Server listening on port 3000");
 });
 
